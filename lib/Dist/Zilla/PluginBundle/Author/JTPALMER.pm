090816af7a254772afa11651f50e29a279f0d6d5
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2012-05-11 17:36:50 -0400

    Rename module

---
+++ lib/Dist/Zilla/PluginBundle/Author/JTPALMER.pm
@@ -0,0 +1,6 @@
+package Dist::Zilla::PluginBundle::Author::JTPALMER;
+use strict;
+use warnings;
+
+1;
+
--- lib/Dist/Zilla/PluginBundle/JTPALMER.pm
+++
@@ -1,6 +0,0 @@
-package Dist::Zilla::PluginBundle::JTPALMER;
-use strict;
-use warnings;
-
-1;
-
